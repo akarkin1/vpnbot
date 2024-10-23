b3b5b78db326a5c3b869738af385254fd9bb1c19
Alexandr Karkin <alexandr.karkin@cat.com>
2024-10-23 05:49:20 +0400

    Tailscale VPN Node:  - add debug print to monitor_connections.sh.

--- docker/scripts/monitor_connections.sh
+++ docker/scripts/monitor_connections.sh
@@ -10,16 +10,19 @@ inactive_time=0
 while true; do
     # Check the number of active connections
     ACTIVE_CONNECTIONS=$(tailscale status | grep 'Active' | wc -l)
+    echo "Tailscale status: $(tailscale status --json)"
 
     if [ "$ACTIVE_CONNECTIONS" -eq 0 ]; then
         # Increment inactive time if there are no active connections
         inactive_time=$((inactive_time + STATUS_CHECK_INTERVAL))
+        echo "No active connections. The node will be terminated in $((INACTIVITY_TIMEOUT-inactive_time)) seconds."
 
         if [ "$inactive_time" -ge "$INACTIVITY_TIMEOUT" ]; then
             echo "No active connections for $INACTIVITY_TIMEOUT seconds. Stopping the Tailscale task..."
             exit 0  # Exit the script, which will stop the container
         fi
     else
+        echo "A user is connect to the Tailscale Node."
         # Reset the inactive time counter if there are active connections
         inactive_time=0
     fi
