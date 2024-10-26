b72ef9b76cace3cb347bf0c61dfea37a8c43c7cd
Alexandr Karkin <alexandr.karkin@cat.com>
2024-10-27 02:59:41 +0400

    Tailscale VPN Node:  - add debug prints to WhiteListAuthenticator.

--- src/main/java/org/github/akarkin1/auth/WhiteListAuthenticator.java
+++ src/main/java/org/github/akarkin1/auth/WhiteListAuthenticator.java
@@ -1,6 +1,7 @@
 package org.github.akarkin1.auth;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.github.akarkin1.config.YamlApplicationConfiguration.AuthConfiguration;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.Map;
 // This should be enough for a TG bot, provided that communication between TG Server and lambda is
 // secured with SSL, and no other connections are allowed by a SecurityGroup,
 // apart from TG API Servers.
+@Log4j2
 @RequiredArgsConstructor
 public class WhiteListAuthenticator implements Authenticator {
 
@@ -17,11 +19,14 @@ public class WhiteListAuthenticator implements Authenticator {
 
   @Override
   public boolean isAllowed(String tgUsername, UserAction action) {
+    log.debug("auth debug: username='{}', action={}, isWhiteListEnabled={}",
+              tgUsername, action, config.isWhiteListEnabled());
     if (!Boolean.TRUE.equals(config.isWhiteListEnabled())) {
       return true;
     }
 
     Map<String, List<UserAction>> usersMap = config.getTgusersWhiteList();
+    log.debug("usersMap: {}", usersMap);
 
     return usersMap.containsKey(tgUsername)
            && usersMap.get(tgUsername).contains(action);
